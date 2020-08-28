87a48d7405f20a528a32ad90a0bcab3408daa3d9
Donobuz <donobuz@gmail.com>
2020-08-27 21:53:16 -0400

    created MainController.java

---
+++ src/main/java/com/CSCI4050/TermProject/CovidWebsite/controllers/MainController.java
@@ -0,0 +1,20 @@
+package com.CSCI4050.TermProject.CovidWebsite.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("name")
+public class MainController {
+
+        @RequestMapping(value="/index", method = RequestMethod.GET)
+        public String showIndexPage(ModelMap model){
+            return "index";
+        }
+
+    }
+
